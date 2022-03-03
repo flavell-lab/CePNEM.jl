8b6ad9aed18558f3e7c38757cc8ad97877aae0f2
goldenratio1618 <adamatanas@gmail.com>
2022-03-03 18:38:18 -0500

    Update run_sbc

--- src/run_sbc.jl
+++ src/run_sbc.jl
@@ -1,8 +1,8 @@
 Gen.@load_generated_functions
 
-n_params = 7
+n_params = 9
 fit_uid = "2021-05-26-07"
-output_path = "/om2/user/aaatanas/gen_output_nl7b/h5/$(ARGS[1]).h5"
+output_path = "/om2/user/aaatanas/gen_output_nl7b_2/h5/$(ARGS[1]).h5"
 path_h5 = "/om2/user/aaatanas/processed_h5/$(fit_uid)-combined_data.h5"
 dict = import_data(path_h5)
 
@@ -20,34 +20,22 @@ h5open(output_path, "w") do f
     f["ground_truth"] = get_free_params(trace, model)
 end
 
-particles_5000 = zeros(2047, n_params)
-@time particles = particle_filter_incremental(5000, v, θh, P, ys, 2047, 1, model)
+particles_5000_2 = zeros(2047, n_params)
+@time particles = particle_filter_incremental(5000, v, θh, P, ys, 2047, 2, model)
 for (i,p) in enumerate(particles)
-    particles_5000[i,:] .= get_free_params(p, model)
+    particles_5000_2[i,:] .= get_free_params(p, model)
 end
 
 h5open(output_path, "r+") do f
-    f["particles_5000"] = particles_5000
+    f["particles_5000_2"] = particles_5000_2
 end
 
-particles_1000 = zeros(511, n_params)
-@time particles = particle_filter_incremental(1000, v, θh, P, ys, 511, 1, model)
-for (i,p) in enumerate(particles)
-    particles_1000[i,:] .= get_free_params(p, model)
-end
-
-h5open(output_path, "r+") do f
-    f["particles_1000"] = particles_1000
-end
-
-particles_1000_10 = zeros(511, n_params)
-@time particles = particle_filter_incremental(1000, v, θh, P, ys, 511, 10, model)
+particles_2000_10 = zeros(1023, n_params)
+@time particles = particle_filter_incremental(2000, v, θh, P, ys, 1023, 10, model)
 for (i,p) in enumerate(particles)
     particles_1000_10[i,:] .= get_free_params(p, model)
 end
 
 h5open(output_path, "r+") do f
-    f["particles_1000_10"] = particles_1000_10
+    f["particles_2000_10"] = particles_2000_10
 end
-
-
